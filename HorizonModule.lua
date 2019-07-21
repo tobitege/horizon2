09a30474fab1ca8254efa4eacacbe19c304d3594
Otixa <otixa.scythe@gmail.com>
2019-07-21 10:54:59 +0300

    Tweaks

--- HorizonModule.lua
+++ HorizonModule.lua
@@ -1,11 +1,6 @@
 HorizonModule = function (name)
     local this = {}
-
-    local mt = {
-        __call = function(ref, ...) ref.Update(...) end,
-        _name = "HorizonModule"
-    }
-    setmetatable(this, mt)
+    setmetatable(this, { __call = function(ref, ...) ref.Update(...) end, _name = "HorizonModule" })
 
     this.Name = name or ""
     this.Dependencies = {}
@@ -14,6 +9,5 @@ HorizonModule = function (name)
     function this.Update(eventType) end
     function this.Register() end
     function this.Unregister() end
-
     return this
 end
