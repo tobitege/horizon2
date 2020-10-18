db174354a97bd5c1ba21e693e0583a495999aff0
Otixa <admin@shadowtemplar.org>
2020-10-18 18:57:59 +0000

    Fixed Altitude reading

--- Modules/CoreModules/Readings.lua
+++ Modules/CoreModules/Readings.lua
@@ -40,7 +40,7 @@ ReadingsModule =
         World.AngularAirFriction = vec3(core.getWorldAirFrictionAngularAcceleration())
 
         -- Ship
-        Ship.Altitude = vec3(core.getAltitude())
+        Ship.Altitude = core.getAltitude()
         Ship.Id = core.getConstructId()
         Ship.Mass = core.getConstructMass()
         Ship.CrossSection = core.getConstructCrossSection()
