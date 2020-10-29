09edb51f7d6f99f27ea119303e2fc5fa553162d6
Otixa <otixa.scythe@gmail.com>
2020-10-29 21:12:18 +0200

    Cleanup submodule leftovers

--- Libs/datalib/Fuels.lua
+++
@@ -1,6 +0,0 @@
-Fuels = {
-    --Mass in kg/L
-    Kergon = {Name="Kergon", Mass=6},
-    Xeron = {Name="Xeron", Mass=0.8},
-    Nitron = {Name="Nitron", Mass=4}
-}
--- Libs/utility-library/RemoteMonitor.lua
+++
@@ -1,7 +0,0 @@
-RemoteMonitor = (function(db)
-
-    for k,v in pairs(_G) do
-        if string.find(k, "Unit_") then databank.setStringValue(k,k) end
-    end
-
-end)(databank)
