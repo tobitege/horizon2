3333e58af5b19dfea82813419a0818d599ec46de
Otixa <admin@shadowtemplar.org>
2021-12-02 05:17:00 +0200

    Build script improvements

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = "Stop"
+$DefaultColor = $Host.UI.RawUI.ForegroundColor
 
 # The file to copy to clipboard after building:
 $FileToCopy = "Standard.json"
@@ -11,29 +12,28 @@ New-Item -Path ./DUnit/ -ItemType Directory -Force > $null
 New-Item -Path ./bin/ -ItemType Directory -Force > $null
 New-Item -Path ./testresults/ -ItemType Directory -Force > $null
 
-# Clean build,test dir
-Remove-Item ./bin/*.*
-Remove-Item ./testresults/*.*
-
 # Ensure that DUBuild is present
 if (-not(Test-Path -Path ./DUBuild/DUBuild.dll -PathType Leaf)) {
     write-host -ForegroundColor Red "`ERROR! DUBuild is not present. Exising...";
     exit
 }
 
-write-host -ForegroundColor Blue "Building...";
-
 # Build
-./DUBuild/DUBuild.exe build -nm -e ./DUBuild,./DUnit,./Tests,./Modules/SpecialisedModules -s ./ -m ./Main*.lua -o ./bin/ | Select-String -CaseSensitive "ERROR"
+write-host -ForegroundColor Blue "Building...";
+$Host.UI.RawUI.ForegroundColor = "Red"
+Remove-Item ./bin/*.*
+dotnet ./DUBuild/DUBuild.dll build -nm -e ./DUBuild,./DUnit,./Tests,./Modules/SpecialisedModules -s ./ -m ./Main*.lua -o ./bin/ | Select-String -CaseSensitive "ERROR"
 if($?) {
     write-host -ForegroundColor Green "Build successful.`n";
+    get-content ./bin/$FileToCopy | set-clipboard;
+    write-host -ForegroundColor Yellow "JSON loaded to clipboard.`n"
+    $Host.UI.RawUI.ForegroundColor = $DefaultColor
 }
 else {
-    write-host -ForegroundColor Red "`nERROR! Build failed. Exiting...";
+    write-host -ForegroundColor Red "ERROR! Build failed. Exiting...";
+    $Host.UI.RawUI.ForegroundColor = $DefaultColor
     exit
 }
-get-content ./bin/$FileToCopy | set-clipboard;
-write-host -ForegroundColor Blue "JSON loaded to clipboard."
 
 # Ensure that DUnit is present
 if (-not(Test-Path -Path ./DUnit/DUnit.dll -PathType Leaf)) {
@@ -41,14 +41,20 @@ if (-not(Test-Path -Path ./DUnit/DUnit.dll -PathType Leaf)) {
     exit
 }
 
-write-host -ForegroundColor Blue "`nRunning tests...";
 
 # Test
-dotnet ./DUnit/DUnit.dll test -s ./bin/*.json -t ./Tests -l ./testresults | Set-Variable -Name "UnitTests" | Select-String "was successful"
+if (Test-Path ./error.log) {
+    Remove-Item -Force ./error.log
+}
+write-host -ForegroundColor Blue "Running tests...";
+$Host.UI.RawUI.ForegroundColor = "Red"
+Remove-Item ./testresults/*.*
+dotnet ./DUnit/DUnit.dll test -s ./bin/*.json -t ./Tests -l ./testresults | Tee-Object -FilePath ./error.log | Select-String -CaseSensitive "ERROR"
 if($?) {
+    Remove-Item -Force ./error.log
     write-host -ForegroundColor Green "Tests finished OK";
 }
 else {
-    write-host -ForegroundColor Red "`nERROR! Tests failed. Error log written to 'error.log'";
-    $UnitTests | out-file error.log
+    write-host -ForegroundColor Red "ERROR! Tests failed. Test log written to 'error.log'";
 }
+$Host.UI.RawUI.ForegroundColor = $DefaultColor
