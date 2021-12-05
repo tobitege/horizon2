a4ca8f5c4f7a27b5b21e4a4c3388b3bd90677175
Ken Hall <eviltek2099@gmail.com>
2021-12-05 19:59:06 +0000

    Patch to correct alt climb when following gravity

--- Modules/OptionalModules/GravityFollow.lua
+++ Modules/OptionalModules/GravityFollow.lua
@@ -4,31 +4,35 @@ GravityFollow = (function()
     local this = HorizonModule("Gravity Follow", "Forces ship to follow gravity down, effectively locking ship pitch to the horizon", "Flush", false)
     this.Tags = "thrust,stability"
     this.Config = {
-        AdjustSpeed = 5
+        AdjustSpeed = 6
     }
     this.Config.Version = "%GIT_FILE_LAST_COMMIT%"
-    
-    function this.Update(eventType, deltaTime)
-        local world = Horizon.Memory.Static.World
-        local ship = Horizon.Memory.Dynamic.Ship
-        local relAccel = Horizon.Memory.Static.Local.Acceleration
-        local relVel = Horizon.Memory.Static.Local.Velocity
-        local dot = relVel:normalize():dot(relAccel:normalize())
-        local offset = 0
-        if dot <= 0 then offset = 1 end
 
-        local maxKinematics = Horizon.Core.getMaxKinematicsParametersAlongAxis("all", {relAccel:normalize():unpack()})
-        if world.AtmosphericDensity > 0 then
-            maxKinematics = math.abs(maxKinematics[1 + offset])
-        else
-            maxKinematics = math.abs(maxKinematics[3 + offset])
-        end
-        local current = relAccel:len() * Horizon.Memory.Static.Ship.Mass
-        local scale = this.Config.AdjustSpeed * math.min(math.max(current / maxKinematics, 0.1), 1) * 10
-
-        ship.Rotation = ship.Rotation + (world.Up:cross(-world.Vertical) * scale)
+    local function transformPitch(pitch)
+        pitch = pitch % 180
+        if pitch > 90 then pitch = pitch - 180 end
+        local mult = 10
+        pitch = math.floor(pitch * mult + 0.5) / mult
+        return pitch
     end
-    
+
+    local function Clamp(n, min, max)
+        return math.min(max, math.max(n, min))
+    end
+
+    function this.Update(eventType)
+        local world = Horizon.Memory.Static.World
+        local staticShip = Horizon.Memory.Static.Ship
+        local ship = Horizon.Memory.Dynamic.Ship
+
+        -- Maximum correction allowed is 1.25/2=0.625 degrees per tick
+        local mod = Clamp(world.VerticalVelocity * this.Config.AdjustSpeed, 0, 1.25)
+        local pitch = transformPitch(staticShip.Pitch) - mod
+
+        local offsetVec = -world.Right * (pitch * constants.deg2rad)
+        ship.Rotation = ship.Rotation + (world.Up:cross(-world.Vertical) - offsetVec)
+    end
+
     Horizon.Emit.Subscribe("GravityFollow", function() this.ToggleEnabled() end)
 
     return this
